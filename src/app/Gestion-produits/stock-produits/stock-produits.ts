400570c403b14efad1809d3ed04d10450ce21189
JcKaps <kapalatajc@gmail.com>
2025-12-28 17:55:51 +0200

    correction du tableau dans porduits disponible en suprimmant view tableau

--- src/app/Gestion-produits/stock-produits/stock-produits.ts
+++ src/app/Gestion-produits/stock-produits/stock-produits.ts
@@ -2,21 +2,29 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ScrollingModule } from '@angular/cdk/scrolling';
 import { ProduitsService } from '../produits-service';
-import { ViewProduitsTables } from "../view-produits-tables/view-produits-tables";
 import { Loading } from "../../loading/loading";
 import { ProductDataSource } from '../product-data-source';
+import { Produit } from '../../Models/produit';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stock-produits',
-  imports: [CommonModule, ScrollingModule, ViewProduitsTables, Loading], 
+  imports: [CommonModule, ScrollingModule, Loading], 
   templateUrl: './stock-produits.html',
   styleUrl: './stock-produits.css',
 })
 export class StockProduits implements OnInit {
   private produitsService = inject(ProduitsService);
+  private router = inject(Router);
   dataSource!: ProductDataSource;
 
   ngOnInit() {
     this.dataSource = new ProductDataSource(this.produitsService);
+  }
+
+  onRowClick(row: Produit | undefined) {
+    if (row && row.id) {
+      this.router.navigate(['produits/detail-produit/', row.id]);
+      }
   }
 }
